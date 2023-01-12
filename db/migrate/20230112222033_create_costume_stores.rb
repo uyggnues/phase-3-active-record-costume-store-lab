02c23a7334883a12e62640b5d79ca9bb20b5c94b
Seunggyu <seunggyul14@icloud.com>
2023-01-12 15:06:16 -0800

    commit tree c975be6f81482d968288b9577d633890e7388a5f

---
+++ db/migrate/20230112222033_create_costume_stores.rb
@@ -0,0 +1,15 @@
+class CreateCostumeStores < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :is_in_business, default: true
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+      t.timestamps
+    end
+  end
+end
